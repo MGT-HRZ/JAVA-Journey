a25e443e9c435f3b2b97aea0aacf8f11aaf44c9f
MGT-HRZ <62929236+MGT-HRZ@users.noreply.github.com>
2022-10-25 18:07:27 +0800

    Update Upper_Lowercase_func.java

--- Exercise 2/String_Functions/Upper_Lowercase_func.java
+++ Exercise 2/String_Functions/Upper_Lowercase_func.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package String_Functions;
 
-/**
- *
- * @author spide
- */
 public class Upper_Lowercase_func {
     public static void main(String args[])
     {
